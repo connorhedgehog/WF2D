e1455ad416dbdf197f2ecee5b931ac32f8457ee9
connorhedgehog <70295743+connorhedgehog@users.noreply.github.com>
2024-11-02 11:39:32 -0500

    Remove players when they disconnect

--- Scripts/world.gd
+++ Scripts/world.gd
@@ -5,9 +5,16 @@ var player_scene : PackedScene = preload("res://Scenes/player.tscn")
 func _ready():
 	if multiplayer.is_server():
 		multiplayer.peer_connected.connect(add_player)
+		multiplayer.peer_disconnected.connect(del_player)
 		add_player()
 	
 func add_player(id = 1):
 	var player = player_scene.instantiate()
 	player.name = str(id)
 	call_deferred("add_child", player)
+
+func del_player(id):
+	rpc("_del_player", id)
+	
+@rpc("any_peer", "call_local") func _del_player(id):
+	get_node(str(id)).queue_free()
